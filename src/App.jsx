939e6f91299895aada45c1bb884b87899a1cabd6
dimitrinossar <dimitri.nossar@gmail.com>
2023-09-08 14:18:25 +1000

    ffmpeg wasm usage example

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { toBlobURL } from '@ffmpeg/util'
+import { useRef, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const ffmpegRef = useRef(new FFmpeg())
 
-  return (
+  const load = async () => {
+    const baseURL = 'https://unpkg.com/@ffmpeg/core-mt@0.12.2/dist/esm'
+    const ffmpeg = ffmpegRef.current
+    ffmpeg.on('log', ({ message }) => {
+      // if (messageRef.current) messageRef.current.innerHTML = message
+      console.log(message)
+    })
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        'application/wasm',
+      ),
+      workerURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.worker.js`,
+        'text/javascript',
+      ),
+    })
+    setLoaded(true)
+  }
+
+  return loaded ? (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Loaded!</h1>
     </>
+  ) : (
+    <button onClick={load}>Load ffmpeg/core</button>
   )
 }
 
